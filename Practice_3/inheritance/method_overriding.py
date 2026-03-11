218f19f7acb620fb91a21fe82181bf332f32d4b8
vladislavmalygin1 <v_malygin@kbtu.kz>
2026-03-11 12:14:07 +0500

    remove comments

--- Practice_3/inheritance/method_overriding.py
+++ Practice_3/inheritance/method_overriding.py
@@ -1,28 +1,27 @@
-# Python program to demonstrate 
-# Defining parent class 
+
 class Parent(): 
 	
-	# Constructor 
+
 	def __init__(self): 
 		self.value = "Inside Parent"
 		
-	# Parent's show method 
+
 	def show(self): 
 		print(self.value) 
 		
-# Defining child class 
+
 class Child(Parent): 
 	
-	# Constructor 
+	
 	def __init__(self): 
-		super().__init__()  # Call parent constructor
+		super().__init__()  
 		self.value = "Inside Child"
 		
-	# Child's show method 
+	
 	def show(self): 
 		print(self.value) 
 		
-# Driver's code 
+
 obj1 = Parent() 
 obj2 = Child() 
 
